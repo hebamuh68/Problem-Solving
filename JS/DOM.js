a9e43ef464ad2a9e0764212de79a407ae47be538
Heba Allah. Hashim <69214737+hebamuh68@users.noreply.github.com>
2022-09-01 15:33:27 +0200

    Update DOM.js

--- JS/DOM.js
+++ JS/DOM.js
@@ -89,4 +89,146 @@ const button = document.getElementById('button');
 
 //====================================================================
 //4. Move element with mouse cursor
+  const element = document.querySelector('#element');
+  const wrapper = document.querySelector('#wrapper');
+  
+  
+  const moveRight = () => {
+    const wrapperWidth = wrapper.getBoundingClientRect().width;
+    const elementWidth = element.getBoundingClientRect().width;
+    const currentPosition = parseInt(element.style.left || 0, 10);
+    if (currentPosition <= wrapperWidth - elementWidth) {
+      element.style.left = `${currentPosition + 1}px`;
+    }
+  };
+  
+  wrapper.addEventListener('mousemove', moveRight);
 
+//====================================================================
+//5. Drop element into another element
+  const dragItem = document.querySelector('#dragItem');
+  const dropArea = document.querySelector('#dropArea');
+  
+  const handleDragOver = (event) => event.preventDefault();
+  dropArea.addEventListener('dragover', handleDragOver);
+  
+  const handleDrop = () => {
+    dropArea.append(dragItem);
+  };
+  
+  dropArea.addEventListener("drop",handleDrop);
+
+=========================================================================================
+DOM manipulation with JavaScript
+==============================================================================
+
+//1. Remove element from the DOM
+  const button = document.querySelector('#button');
+  
+  const removeRedCircle = () => {
+    const redCircle = document.querySelector('#red');
+    redCircle.parentNode.removeChild(redCircle);
+  };
+
+//====================================================================
+//2. Change id of HTML element
+  const button = document.querySelector('#wrapper button');
+  
+  const changeInput = () => {
+    const input = document.querySelector('#inputEl');
+    if(input) {
+      input.value = 'Hello World';
+    }
+  };
+  
+  button.addEventListener('click', changeInput);
+  
+  document.querySelector('#wrapper input').setAttribute('id', 'inputEl');
+
+//====================================================================
+//3. Wrap element in div
+
+
+//====================================================================
+//4. Create new list item and add to DOM
+  const button = document.getElementById('button');
+  button.addEventListener('click' , () => {
+    const input = document.getElementById('input');
+    const list = document.getElementById('list');
+    const newItem = document.createElement('li');
+    newItem.innerText = input.value;
+    list.append(newItem);
+  });
+
+
+=========================================================================================
+DOM fundamentals
+==============================================================================
+  
+//1. Check the checkbox
+  const button = document.getElementById('button');
+  button.addEventListener('click', () => {
+    const checkbox = document.getElementById('checkbox');
+    checkbox.checked = true;
+  });
+
+//====================================================================
+//2. Get full-name from inputs
+const button = document.getElementById('button');
+button.addEventListener('click', () => {
+  // type in your code here
+  const firstName = document.getElementById('firstName');
+  const lastName = document.getElementById('lastName');
+  const target = document.getElementById('fullName');
+  target.value = firstName.value + lastName.value;
+});
+
+//====================================================================
+//3. Increment the counter on button click
+
+//====================================================================
+//4. Input filter list
+  const input = document.getElementById('input');
+  input.addEventListener('input', () => {
+    const listItems = document.querySelectorAll('#list li');
+    listItems.forEach(li => {
+      if(li.innerText.includes(input.value)) return li.hidden = false;
+      return li.hidden = true;
+    })
+  });
+
+//====================================================================
+//5. Pop the balloons
+
+  const list = document.getElementById('list');
+  const handleHover = event => {
+    if(event.target !== list) {
+      event.target.style.visibility = 'hidden';
+    }
+  };
+  
+  list.addEventListener('mouseover', handleHover);
+  
+=========================================================================================
+Recursive functions
+==============================================================================
+
+
+  const button = document.getElementById('button');
+  let stopped = false;
+   
+  function move(isReturning) {
+   const width = button.parentNode.clientWidth;
+   const left = parseInt(button.style.left , 10) || 0;
+   if (!stopped) {
+      button.style.left = (isReturning ? left - 1 : left + 1) + 'px';
+      setTimeout(() => move ((isReturning && left > 0) || left === width - button.clientWidth), 10);
+   };
+  };
+   
+  move();
+   
+  button.addEventListener('click', () => {
+    stopped = !stopped;
+    move();
+  })
